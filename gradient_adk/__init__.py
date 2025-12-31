7c7164d0be29958cd378619f50cf17cbf27c0343
Tyler Gillam <tgillam@digitalocean.com>
2025-12-31 08:38:39 -0600

    Add functions for adding spans

--- gradient_adk/__init__.py
+++ gradient_adk/__init__.py
@@ -4,17 +4,25 @@ and the CLI (gradient command).
 """
 
 from .decorator import entrypoint
-from .tracing import (  # manual tracing decorators
+from .tracing import (  # manual tracing decorators and functions
     trace_llm,
     trace_retriever,
     trace_tool,
+    add_llm_span,
+    add_retriever_span,
+    add_tool_span,
 )
 
 __all__ = [
     "entrypoint",
+    # Decorators
     "trace_llm",
     "trace_retriever",
     "trace_tool",
+    # Functions
+    "add_llm_span",
+    "add_retriever_span",
+    "add_tool_span",
 ]
 
 __version__ = "0.0.5"
